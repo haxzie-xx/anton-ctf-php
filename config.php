985850e9670259b3aadcd871d72e1f6460866c1c
Musthaq Ahamad <musthu.gm@gmail.com>
2018-11-22 09:04:50 +0530

    Fix db connection

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "localhost";
 $username = "root";
-$password = "";
+$password = "root";
 $database = "anton";
 // Create connection
 $conn = new mysqli($servername, $username, $password, $database);
